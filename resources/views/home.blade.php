bc424d93d01379606a37d11a91e84336f9f5e34b
Ada Mitchel Tacal <134472178+eidada116@users.noreply.github.com>
2025-07-23 15:50:24 +0800

    Updated Profile and Homepage Functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,41 @@
 @extends('layouts.app')
 @section('content')
-    <header>
-        <h1>Featured</h1>
-    </header>
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+
+    <div class="home-content">
+
+        <div class="left-content">
+            <div class="recommended">
+                <h2>Recommended For You</h2>
+                <div class="links">
+                    @foreach ($recommended as $reco)
+                    <div class="box">
+                        <a href="/blogpost/{{ $reco->blogpost_id }}">{{ $reco->title }}</a>
+                    </div>       
+                    @endforeach
+                </div>
+            </div>
+
+            <h2>LATEST Today</h2>
+            <div class="latest">
+                @foreach ($randomPosts as $post)
+                    <div class="latest-box">
+                        <img src="{{ $post->image_path }}"  width="300">
+                        <a href="/blogpost/{{ $post->blogpost_id }}"><h2>{{$post->title}}</h2></a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div class="right-content">
+            <h2>Exclusive News</h2>
+            @foreach ($randomPosts as $post)
+                <a href="/blogpost/{{ $post->blogpost_id }}">
+                {{ $post->title }}
+                </a>
+            @endforeach
+        </div>
+    </div>
+    
+
 @endsection
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User Profile</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>{{ $user->first_name }} {{ $user->last_name }}</h1>
     <p>Email: {{ $user->email }}</p>
     <p>Username: {{ $user->username }}</p>
     <p>Role: {{ $user->role }}</p>
-    <img src="{{ $user->profile_picture }}" alt="Profile Photo" width="100">
-</body>
-</html>
+    <img src="{{ $user->profile_picture}}" 
+          width="100">
+@endsection
