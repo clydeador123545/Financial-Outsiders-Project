5e6856a371d60f8c774ef5ed7874f841931de558
CIITSHSDTP00\CIIT <anamggss1227@gmail.com>
2025-07-31 13:07:02 +0800

    You are my sunshine~ My only sunshine~

--- resources/views/blogpost.blade.php
+++ resources/views/blogpost.blade.php
@@ -30,11 +30,30 @@
         <div class="suggested-container">
             <div class="imgpad"></div>
             <div class="suggested">
-                <h3>Related</h3>
+                <h1 style="text-align: center;">Related</h1>
+
                 @foreach ($otherPosts as $post)
+
+                <div class="latest-box" style="display: flex; align-items: center; gap: 5px; margin-bottom: 30px; flex-direction: row;">
+                    @php
+                    $files = glob(public_path('images/artimages/*.png'));
+                    $path = count($files) ? 'images/artimages/' . basename($files[array_rand($files)]) : null;
+                    $imagePath = $path ?? 'images/artimages/default.png';
+                    @endphp
+
+                    @if ($path)
+                    <div
+                        style="width: clamp(120px, 20vw, 120px); height: clamp(100px, 20vw, 100px); overflow: hidden; flex-shrink: 0; box-shadow: 6px 6px 0px 0px rgba(0, 0, 0, 0.2);">
+                        <img src="{{ asset($path) }}" alt="Random Art"
+                            style="width: clamp(120px, 20vw, 120px); height: clamp(100px, 20vw, 100px); object-fit: cover; display: block; ">
+                    </div>
+                    @endif               
+
                 <a href="/blogpost/{{ $post->blogpost_id }}">
                     {{ $post->title }}
                 </a>
+
+                </div>
                 @endforeach
             </div>
         </div>
