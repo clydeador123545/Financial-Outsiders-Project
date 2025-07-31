c329b17d0510739e0b855d4435adfc7c8d298ec5
CIITSHSDTP00\CIIT <anamggss1227@gmail.com>
2025-07-31 09:23:09 +0800

    A

--- resources/views/blogpost.blade.php
+++ resources/views/blogpost.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <link rel="stylesheet" href="{{ asset('css/blogpost.css') }}">
+<link rel="stylesheet" href="{{ asset('css/blogpost.css') }}">
 
-    <div class="main-content">
+<div class="main-content">
+
+    <div class="blog-container">
         <div class="blog">
-        <h1>{{ $blogpost->title }}</h1>
-        <div class="author-details">
-            <p>Author: {{ $blogpost->author->first_name }} {{ $blogpost->author->last_name }}</p>
+
+            <h1>{{ $blogpost->title }}</h1>
+            <div class="author-details">
+                <p>Author: {{ $blogpost->author->first_name }} {{ $blogpost->author->last_name }}</p>
+            </div>
+            <p>{{ $blogpost->content }}</p>
         </div>
-        <p>{{ $blogpost->content }}</p>
-        </div>
-        
-        <div class="suggested">
-            <h3>Related</h3>
-            @foreach ($otherPosts as $post)
-                    <a href="/blogpost/{{ $post->blogpost_id }}">
-                        {{ $post->title }}
-                    </a>
-            @endforeach         
-        </div>
-        
     </div>
-    
+
+    <div class="suggested">
+        <h3>Related</h3>
+        @foreach ($otherPosts as $post)
+        <a href="/blogpost/{{ $post->blogpost_id }}">
+            {{ $post->title }}
+        </a>
+        @endforeach
+    </div>
+
+</div>
+
 @endsection
